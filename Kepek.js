bbad5c2cc1c7b1367e47cfc2e12e4ee47f5ff2ab
Csillag Gergő Zsolt (SZF_N_2025_2027) <csillag.gergo.zsolt@diak.szamalk-szalezi.hu>
2026-03-31 12:45:40 +0200

    elso commit

---
+++ Kepek.js
@@ -0,0 +1,17 @@
+export default class Kepek {
+  #lista = {};
+
+  constructor(lista = { kep, nev }, szuloElem) {
+    this.#lista = lista;
+    this.szuloElem = szuloElem;
+    this.megjelenit();
+  }
+
+  getLista(){
+    return this.#lista
+  }
+
+  megjelenit(){
+    
+  }
+}
